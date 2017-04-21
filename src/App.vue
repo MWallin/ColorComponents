e7c713e807d4b346cda33b444cd61a0bdf336997
Mikael Wallin <mikael.wallin@gmail.com>
2017-04-21 20:49:44 +0200

    second first commit

---
+++ src/App.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col-xs-12">
+        <br>
+          <button @click="selectedColor = 'appBlue'" class="btn btn-primary">Load Blue Template</button>
+          <button @click="selectedColor = 'appGreen'"class="btn btn-success">Load Green Template</button>
+          <button @click="selectedColor = 'appRed'"class="btn btn-danger">Load Red Template</button>
+        <hr>
+
+          <component :is="selectedColor">
+            <h1>{{ randomString }}</h1>
+          </component>
+
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import Blue from "./components/Blue.vue"
+  import Green from "./components/Green.vue"
+  import Red from "./components/Red.vue"
+
+  export default {
+    data: function() {
+      return {
+        selectedColor: null,
+        randomString : "This is me!!"
+
+      }
+    },
+
+    components: {
+      appBlue : Blue,
+      appGreen: Green,
+      appRed  : Red
+    }
+  }
+</script>
+
+<style>
+</style>
